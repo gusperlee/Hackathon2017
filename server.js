3262bd479fd80088779addbe0eac8cdf16c1218c
funkyfisch <funkyfisch@gmail.com>
2017-05-06 19:37:44 +0200

    saving analysis in json file

--- server.js
+++ server.js
@@ -4,8 +4,28 @@ var fs = require("fs");
 
 var text = fs.readFileSync('sample text','utf8')
 
+
+vocRichness = function(obj) {
+	var key = 0;
+	var uniqueWords = 0;
+	for (key in obj) {
+		if (obj.hasOwnProperty(key)) {
+			uniqueWords += obj[key].length;
+			}
+		}
+	return uniqueWords;
+};
+
+saveSongStats = function(obj) {
+		var stream = fs.createWriteStream("database.json");
+stream.once('open', function(fd) {
+  stream.write("" + vocRichness(TFIDF.countTermFrequencies(obj)))
+});
+};
+
 console.log(TFIDF.countTermFrequencies(text));
-console.log(text);
+console.log(vocRichness(TFIDF.countTermFrequencies(text)));
+saveSongStats(text);
 
 
 
