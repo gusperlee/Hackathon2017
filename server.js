cc219f7472829cdd5c48e8e11b093f59788e4c61
funkyfisch <funkyfisch@gmail.com>
2017-05-06 18:18:59 +0200

    analyser ready

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-var wordFrequencyAnalyzer = require("word-frequency-analyzer");
+var nGrams = require("word-ngrams");
+var TFIDF = require("document-tfidf");
 var fs = require("fs");
+
 var text = fs.readFileSync('sample text','utf8')
+
+console.log(TFIDF.countTermFrequencies(text));
 console.log(text);
+
+
+
