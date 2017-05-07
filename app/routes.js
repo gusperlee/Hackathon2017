1509337358f16b0939f269b14f4332e6f2329f31
Unknown <funkyfisch@gmail.com>
2017-05-07 00:39:15 -0700

    last few steps

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,19 @@
 var fs = require("fs");
 var path = require('path');
+var nGrams = require("word-ngrams");
+var TFIDF = require("document-tfidf");
+
+vocRichness = function(obj) {
+	var key = 0;
+	var uniqueWords = 0;
+	for (key in obj) {
+		if (obj.hasOwnProperty(key)) {
+			uniqueWords += obj[key].length;
+			}
+		}
+	return uniqueWords;
+};
+
 
 module.exports = function(app) {
   app.get('/api/stats', function(req, res) {
@@ -15,9 +29,12 @@ module.exports = function(app) {
   app.post('/api/lyrics', function(req, res) {
     //do the overlay with the standard stuff
     console.log("someonesubmittedlyrics");
+    var lyrics = req.body.lyrics;
+    console.log(vocRichness(TFIDF.countTermFrequencies(lyrics)));
+    res.send({ score : vocRichness(TFIDF.countTermFrequencies(lyrics))});
   })
   app.get('*', function(req,res) {
     res.sendFile(path.resolve('public/views/index.html'));
   });
-  
+
 };
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -3,13 +3,14 @@ angular.module('MainCtrl', []).controller('MainController', function($scope, $lo
 
   getScores = function() {
   			$scope.scores = "";
-  		stuff.getScores().then(function(data){
+  		  stuff.getScores().then(function(data){
   			$scope.scores = data.data;
         $scope.labels = getYears($scope.scores);
         $scope.data = getRates($scope.scores);
   		});
   	};
-
+    $scope.toggle1 = false;
+    $scope.toggle2 = true;
     getYears = function(obj) {
     	var name;
     	var years = [];
@@ -32,9 +33,70 @@ angular.module('MainCtrl', []).controller('MainController', function($scope, $lo
     };
 
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+     $scope.series = ['Series A']
     $scope.data = [65, 59, 80, 81, 56, 55, 40];
-getScores();
+    $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+ $scope.options = {
+   scales: {
+     yAxes: [
+       {
+         id: 'y-axis-1',
+         type: 'linear',
+         display: true,
+         position: 'left'
+       },
+       {
+         id: 'y-axis-2',
+         type: 'linear',
+         display: true,
+         position: 'right'
+       }
+     ]
+   }
+ };
 
+    getScores();
+    var customLyrics = "default";
+    $scope.saveLyrics = function() {
+        customLyrics = $scope.form;
+    };
+    $scope.submitForm = function() {
+        console.log("posting data....");
+        customLyrics = $scope.form;
+        stuff.submitLyrics(customLyrics).then(function(data){
+          $scope.labels2 = data.data.score;
+          console.log($scope.labels2);
+          $scope.labels3 = [];
+          for (name in $scope.data) {
+            if ($scope.data.hasOwnProperty(name)) {
+        			$scope.labels3.push($scope.labels2);
+        			}
+            }
+            $scope.labels3.push(120);
+            $scope.labels3.push(0);
+          $scope.data = [
+      $scope.data,
+      $scope.labels3
+    ];
+
+    		});
+
+  console.log($scope.labels2);
+
+
+        $scope.toggle2 = false;
+        $scope.toggle1 = true;
+
+
+    };
+
+    $scope.tryAnother = function() {
+      $scope.form = "";
+      $scope.toggle1 = false;
+      $scope.toggle2 = true;
+      getScores();
+
+    }
 
 
 });
